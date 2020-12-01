e4e2829571d344fac5fd535de21e475ae09fd305
Gobind Puniani <gobindpuniani@gmail.com>
2020-12-01 00:31:17 -0800

    Content component built

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
+import POPOSList from './POPOSList';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
       <img src={logo} alt='React Logo' />
     </div>
-    
   );
 }
 
