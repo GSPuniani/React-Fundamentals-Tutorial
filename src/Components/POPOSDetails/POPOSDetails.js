bd1d0be0461554ed0ec4df6b07bc706c6a72f94b
Gobind Puniani <gobindpuniani@gmail.com>
2020-12-06 01:39:12 -0800

    Styled the details page

--- src/Components/POPOSDetails/POPOSDetails.js
+++ src/Components/POPOSDetails/POPOSDetails.js
@@ -1,27 +1,27 @@
 // src/POPOSDetails.js
 
-import React from 'react'
-
-import data from '../../sfpopos-data.json'
+import React from 'react';
+import data from '../../sfpopos-data.json';
+import './POPOSDetails.css'
 
 function POPOSDetails(props) {
   const { id } = props.match.params // Location index
   const { images, title, desc, address, hours, features, geo, website } = data[id]
 
   return (
-    <div>
-      <div>
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
         <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt="POPOS"/>
       </div>
 
-      <div>
-        <h1>{ title }</h1>
-        <p>{ desc }</p>
-        <p>{ address }</p>
-        <p>{ hours }</p>
-        <p>{ features }</p>
-        <p>{ geo.lat } { geo.lon }</p>
-        <p>{ website }</p>
+      <div className="POPOSDetails-info">
+        <h1 className="POPOSDetails-title">{ title }</h1>
+        <p className="POPOSDetails-desc">{ desc }</p>
+        <p className="POPOSDetails-address">{ address }</p>
+        <p className="POPOSDetails-hours">{ hours }</p>
+        <p className="POPOSDetails-features">{ features }</p>
+        <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
+        <p className="POPOSDetails-website">{ website }</p>
       </div>
 
     </div>
